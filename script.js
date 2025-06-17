5a1a91a0837a5445eac37c2d7134d7091f7e7a0f
CollectionofEG <148367269+Pepperhfj@users.noreply.github.com>
2025-06-17 17:13:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// for Slushy  <3   !
+
+const plusButton = document.getElementById('increaser');
+const minusButton = document.getElementById('decreaser');
+
+const painLvls = document.getElementById('lvl');
+
+plusButton.addEventListener ("click", () =>
+    {painLvls.textContent++});
+
+minusButtonButton.addEventListener ("click", () =>
+    {painLvls.textContent--});
+
+// End of line >:3
+
+
+
+
